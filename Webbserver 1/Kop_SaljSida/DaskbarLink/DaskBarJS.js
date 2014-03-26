39420be03505d30f0d65e27e0acd3c7a22b296cf
Tjoppe <bart.102@hotmail.com>
2014-03-26 11:43:49 +0100

    Köp & Sälj
    Hjälp

---
+++ Webbserver 1/Kop_SaljSida/DaskbarLink/DaskBarJS.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../Scripts/jquery-ui-1.8.20.min.js" />
+
+$(document).ready(function() {
+    $(document).scroll(function() {
+        checkOffset();
+    });
+
+    function checkOffset() {
+        if ($('#social-float').offset().top + $('#social-float').height() >= $('#footer').offset().top - 10)
+            $('#social-float').css('position', 'fixed');
+
+        if ($(document).scrollTop() + window.innerHeight < $('#footer').offset().top)
+            $('#social-float').css('position', 'absolute'); // restore when you scroll up
+    }
+
+
+
+});
